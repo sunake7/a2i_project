468f273b5bd6a61132a1088db63ea0514e868f33
hoon <hooncoding@gmail.com>
2024-09-02 12:32:07 +0900

    blur_added inference interval

--- example/android/app/src/main/kotlin/com/ultralytics/example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ultralytics.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- example/android/app/src/main/kotlin/com/ultralytics/yolo_flutter_app_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ultralytics.yolo_flutter_app_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
